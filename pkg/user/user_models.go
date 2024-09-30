ff118fff299b2a29e4c6000cde172f0edfd4c4c3
Asmith Dahjer <asmith200205@gmail.com>
2024-09-29 22:10:02 -0500

    add user models

---
+++ pkg/user/user_models.go
@@ -0,0 +1,19 @@
+package user
+
+type RegisterUserModel struct {
+	UserName string `json:"userName" validate:"required,min=4"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=8"`
+}
+
+type LoginUserModel struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=8"`
+}
+
+type UserModel struct {
+	Id       string `json:"id"`
+	UserName string `json:"userName"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
